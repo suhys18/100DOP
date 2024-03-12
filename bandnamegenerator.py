34bb389ad350a55777db7c2ed50af7dab974b1ea
suhys18 <73975403+suhys18@users.noreply.github.com>
2024-03-13 02:11:13 +0530

    Add files via upload

---
+++ bandnamegenerator.py
@@ -0,0 +1,7 @@
+print("HELLO AND WELCOME TO BAND NAME GENERATOR:")
+pet_name = input("WHAT IS THE NAME OF YOUR PET:\n")
+city = input("WHICH CITY YOU GROW UP IN?:\n")
+
+band_name = '"' + pet_name + " " +  city + '"'
+print("HURRAY YOU GOT YOUR BAND NAME, " + band_name + " ,THAT'S DAMN COOL!!") 
+
